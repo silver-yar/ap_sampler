a250cbb4c403ac1b24ab13b7f41e8f5af716f452
Johnathan Handy <42393941+silver-yar@users.noreply.github.com>
2020-06-09 15:21:35 -0400

    implemented flexbox

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,17 +13,17 @@
 
 //==============================================================================
 Ap_samplerAudioProcessorEditor::Ap_samplerAudioProcessorEditor (Ap_samplerAudioProcessor& p)
-    : AudioProcessorEditor (&p), interface_(p), processor (p)
+    : AudioProcessorEditor (&p),
+      interfaceView_(p),
+      processor (p)
 {
-    addAndMakeVisible(interface_);
-
-    // TODO: Make the top parent component resizable : DONE
     setResizable(true, true);
     // TODO: Set max width and height using monitor size
     setResizeLimits(800, 400, 1600, 800);
 
-
-    setSize(800, 400);
+    setupFlexBoxes();
+    setupFlexItems();
+    setSize(800, 450);
 }
 
 Ap_samplerAudioProcessorEditor::~Ap_samplerAudioProcessorEditor()
@@ -32,27 +32,51 @@ Ap_samplerAudioProcessorEditor::~Ap_samplerAudioProcessorEditor()
 
 //==============================================================================
 
+void Ap_samplerAudioProcessorEditor::resized() {
+    oFlexBox_.performLayout (getFlexBounds().toNearestInt());
+}
+
 void Ap_samplerAudioProcessorEditor::paint (Graphics& g)
 {
-    // Background Layout
-    g.fillAll(main_blue_);
-    g.setColour(sec_blue_);
-    g.fillRect(shadow_bounds_);
-
-    // TODO: USE Flexbox to create layout for containers
+    g.fillAll (Colours::dodgerblue.darker(0.5f));
+    g.setColour (Colours::dodgerblue);
+    g.fillRect(getFlexBounds());
 }
 
-void Ap_samplerAudioProcessorEditor::resized()
+Rectangle<float> Ap_samplerAudioProcessorEditor::getFlexBounds() const
 {
-    shadow_bounds_.setWidth(getWidth());
-    FlexBox flexBox;
-
-    flexBox.flexDirection = FlexBox::Direction::columnReverse;
-    flexBox.alignContent = FlexBox::AlignContent::flexStart;
-    Array<FlexItem> flex_items;
-    flex_items.add(FlexItem(getWidth() * 0.625f, getHeight() * 0.25f, interface_));
-    //flex_items.add(FlexItem(getWidth(), getHeight() * 0.1f));
-    flexBox.items.addArray(flex_items);
-
-    flexBox.performLayout(getLocalBounds().reduced(10.0f));
+    return getLocalBounds().withTrimmedTop (getHeight() * .15f).toFloat();
 }
+
+void Ap_samplerAudioProcessorEditor::setupFlexBoxes()
+{
+    iFlexBox_.flexDirection = FlexBox::Direction::column;
+    iFlexBox_.justifyContent = FlexBox::JustifyContent::spaceAround;
+    iFlexBox_.alignItems = FlexBox::AlignItems::center;
+    oFlexBox_.flexDirection = FlexBox::Direction::row;
+    oFlexBox_.justifyContent = FlexBox::JustifyContent::spaceAround;
+    oFlexBox_.alignItems = FlexBox::AlignItems::stretch;
+}
+
+void Ap_samplerAudioProcessorEditor::setupFlexItems()
+{
+    addItem (iFlexBox_, sampleView_, 2, 10);
+    addItem (iFlexBox_, interfaceView_, 1, 10);
+    addFlex (oFlexBox_, iFlexBox_, 2);
+    addItem (oFlexBox_, paramView_, 1, 10);
+}
+
+void Ap_samplerAudioProcessorEditor::addItem(FlexBox& flexBox, Component& item, float grow, float margin)
+{
+    flexBox.items.add (FlexItem (item).withFlex (grow, 0, 10).withMargin(FlexItem::Margin(margin)));
+    auto& flexItem = flexBox.items.getReference (flexBox.items.size() - 1);
+    flexItem.associatedComponent = &item;
+    addAndMakeVisible (item);
+}
+
+void Ap_samplerAudioProcessorEditor::addFlex(FlexBox& parentFlexBox, FlexBox& childFlexBox, float grow)
+{
+    parentFlexBox.items.add (FlexItem (childFlexBox).withFlex (grow, 0, 10));
+}
+
+
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,6 +13,8 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 #include "InterfaceView.h"
+#include "SampleView.h"
+#include "ParamView.h"
 
 //==============================================================================
 /**
@@ -27,15 +29,30 @@ public:
     void paint (Graphics&) override;
     void resized() override;
 
+    //==============================================================================
+    Rectangle<float> getFlexBounds() const;
+    void setupFlexBoxes(); // Setup for the inner and outer flex boxes
+    void setupFlexItems(); // Setup for the inner and outer flex items
+    void addItem(FlexBox& flexBox, Component& item, float grow, float margin); // Add a component to a flex box
+    static void addFlex(FlexBox& parentFlexBox, FlexBox& childFlexBox, float grow); // Add a flex box to a flex box
+
 private:
-    Rectangle<float> shadow_bounds_ {0.0f, 0.0f, 800.0f, 50.0f};
+//    Rectangle<float> shadow_bounds_ {0.0f, 0.0f, (float)getWidth(), 50.0f};
+//    Rectangle<float> front_bounds_ {0.0f, 50.0f, (float)getWidth(), 350};
+//    Rectangle<float> frontFace_;
+//    Rectangle<float> topFace_;
 
     // Colour Palette
     Colour main_blue_{ 0xff3056bf };
     Colour sec_blue_{0xff203980};
 
+    //FlexBoxes
+    FlexBox iFlexBox_, oFlexBox_;
+
     // Child Components
-    InterfaceView interface_;
+    InterfaceView interfaceView_;
+    SampleView sampleView_;
+    ParamView paramView_;
 
     Ap_samplerAudioProcessor& processor;
 
