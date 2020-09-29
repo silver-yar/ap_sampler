1827128346f49fc537575c3ac48b245417b4cb1e
Johnathan Handy <42393941+silver-yar@users.noreply.github.com>
2020-09-29 19:50:36 -0400

    fixed formatting issue with LabelSlider save

--- Source/LabelSlider.cpp
+++ Source/LabelSlider.cpp
@@ -1,17 +1,17 @@
 /*
-  ==============================================================================
+  =============================================================================
 
     LabelSlider.cpp
-    Created: 24 Sep 2020 11:35:41pm
+    Created: 24 Sep 2020 11:35:41p
     Author:  Johnathan Handy
 
-  ==============================================================================
+  =============================================================================
 */
 
 #include <JuceHeader.h>
 #include "LabelSlider.h"
 
-//==============================================================================
+//=============================================================================
 LabelSlider::LabelSlider()
 {
     slider.setSliderStyle (Slider::SliderStyle::RotaryVerticalDrag);
@@ -39,8 +39,9 @@ void LabelSlider::paint (juce::Graphics& g)
 void LabelSlider::resized()
 {
     auto localBounds = getLocalBounds();
-    auto label_ratio = 0.20;
+    auto label_ratio = 0.2;
 
     label.setBounds (localBounds.removeFromTop (localBounds.getHeight() * label_ratio));
     slider.setBounds (localBounds);
 }
+
